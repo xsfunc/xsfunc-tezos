25bb6a09b05325d63c221cbaaf118a1487509400
xsfunc <xsfunc@gmail.com>
2022-09-30 19:56:11 +0300

    chore: add integrations to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import image from '@astrojs/image'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+const site = process.env.SITE_URL || 'https://xsfunc.xyz'
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  site,
+  integrations: [sitemap(), tailwind(), image()],
+})
